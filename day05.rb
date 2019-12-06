0214b6d1a79299277c2282b4a5eefb85ae1332b6
Philippe Creux <pcreux@gmail.com>
2019-12-06 15:04:14 -0800

    incodecomputer class...

--- day05.rb
+++ day05.rb
@@ -1,51 +1,59 @@
 INPUT = [3,225,1,225,6,6,1100,1,238,225,104,0,1101,9,90,224,1001,224,-99,224,4,224,102,8,223,223,1001,224,6,224,1,223,224,223,1102,26,62,225,1101,11,75,225,1101,90,43,225,2,70,35,224,101,-1716,224,224,4,224,1002,223,8,223,101,4,224,224,1,223,224,223,1101,94,66,225,1102,65,89,225,101,53,144,224,101,-134,224,224,4,224,1002,223,8,223,1001,224,5,224,1,224,223,223,1102,16,32,224,101,-512,224,224,4,224,102,8,223,223,101,5,224,224,1,224,223,223,1001,43,57,224,101,-147,224,224,4,224,102,8,223,223,101,4,224,224,1,223,224,223,1101,36,81,225,1002,39,9,224,1001,224,-99,224,4,224,1002,223,8,223,101,2,224,224,1,223,224,223,1,213,218,224,1001,224,-98,224,4,224,102,8,223,223,101,2,224,224,1,224,223,223,102,21,74,224,101,-1869,224,224,4,224,102,8,223,223,1001,224,7,224,1,224,223,223,1101,25,15,225,1101,64,73,225,4,223,99,0,0,0,677,0,0,0,0,0,0,0,0,0,0,0,1105,0,99999,1105,227,247,1105,1,99999,1005,227,99999,1005,0,256,1105,1,99999,1106,227,99999,1106,0,265,1105,1,99999,1006,0,99999,1006,227,274,1105,1,99999,1105,1,280,1105,1,99999,1,225,225,225,1101,294,0,0,105,1,0,1105,1,99999,1106,0,300,1105,1,99999,1,225,225,225,1101,314,0,0,106,0,0,1105,1,99999,1008,226,677,224,1002,223,2,223,1005,224,329,1001,223,1,223,1007,677,677,224,102,2,223,223,1005,224,344,101,1,223,223,108,226,677,224,102,2,223,223,1006,224,359,101,1,223,223,108,226,226,224,1002,223,2,223,1005,224,374,1001,223,1,223,7,226,226,224,1002,223,2,223,1006,224,389,1001,223,1,223,8,226,677,224,1002,223,2,223,1006,224,404,1001,223,1,223,107,677,677,224,1002,223,2,223,1006,224,419,101,1,223,223,1008,677,677,224,102,2,223,223,1006,224,434,101,1,223,223,1107,226,677,224,102,2,223,223,1005,224,449,1001,223,1,223,107,226,226,224,102,2,223,223,1006,224,464,101,1,223,223,107,226,677,224,102,2,223,223,1005,224,479,1001,223,1,223,8,677,226,224,102,2,223,223,1005,224,494,1001,223,1,223,1108,226,677,224,102,2,223,223,1006,224,509,101,1,223,223,1107,677,226,224,1002,223,2,223,1005,224,524,101,1,223,223,1008,226,226,224,1002,223,2,223,1005,224,539,101,1,223,223,7,226,677,224,1002,223,2,223,1005,224,554,101,1,223,223,1107,677,677,224,1002,223,2,223,1006,224,569,1001,223,1,223,8,226,226,224,1002,223,2,223,1006,224,584,101,1,223,223,1108,677,677,224,102,2,223,223,1005,224,599,101,1,223,223,108,677,677,224,1002,223,2,223,1006,224,614,101,1,223,223,1007,226,226,224,102,2,223,223,1005,224,629,1001,223,1,223,7,677,226,224,1002,223,2,223,1005,224,644,101,1,223,223,1007,226,677,224,102,2,223,223,1005,224,659,1001,223,1,223,1108,677,226,224,102,2,223,223,1006,224,674,101,1,223,223,4,223,99,226]
 
-def run(state, position: 0)
-  #display(state)
 
-  full_opcode = state[position]
-  padded_opcode = full_opcode.to_s.rjust(5, "0")
-  mode_1 = padded_opcode[2].to_i
-  mode_2 = padded_opcode[1].to_i
-  mode_3 = padded_opcode[0].to_i
-  opcode = padded_opcode[3..4].to_i
+class IntcodeComputer
+  attr_reader :state, :input, :position
 
-  output = case opcode
-  when 1
-    opcode, input_1_pos, input_2_pos, output_pos = state[position..position + 3]
-    input_1 = mode_1 == 0 ? state[input_1_pos] : input_1_pos
-    input_2 = mode_2 == 0 ? state[input_2_pos] : input_2_pos
-    output = input_1 + input_2
-    # output is always in position mode
-    state[output_pos] = output
-    run(state, position: position + 4)
-  when 2
-    opcode, input_1_pos, input_2_pos, output_pos = state[position..position + 3]
-    input_1 = mode_1 == 0 ? state[input_1_pos] : input_1_pos
-    input_2 = mode_2 == 0 ? state[input_2_pos] : input_2_pos
-    output = input_1 * input_2
-    # output is always in position mode
-    state[output_pos] = output
-    run(state, position: position + 4)
-  when 3
-    input = 1
-    opcode, output_pos = state[position..position + 1]
-    state[output_pos] = input
-    run(state, position: position + 2)
-  when 4
-    opcode, output_pos = state[position..position + 1]
-    puts state[output_pos]
-    run(state, position: position + 2)
-  when 99
-    return state[0]
-  else
-    raise "invalid opcode #{opcode.inspect}"
+  def initialize(state: INPUT, input: 1, position: 0)
+    @state = state
+    @input = input
+    @position = position
+  end
+
+  def run
+    full_opcode = state[position]
+    padded_opcode = full_opcode.to_s.rjust(5, "0")
+    mode_1 = padded_opcode[2].to_i
+    mode_2 = padded_opcode[1].to_i
+    mode_3 = padded_opcode[0].to_i
+    opcode = padded_opcode[3..4].to_i
+
+    output = case opcode
+    when 1
+      opcode, input_1_pos, input_2_pos, output_pos = state[position..position + 3]
+      input_1 = mode_1 == 0 ? state[input_1_pos] : input_1_pos
+      input_2 = mode_2 == 0 ? state[input_2_pos] : input_2_pos
+      output = input_1 + input_2
+      # output is always in position mode
+      state[output_pos] = output
+      IntcodeComputer.new(state: state, position: position + 4).run
+    when 2
+      opcode, input_1_pos, input_2_pos, output_pos = state[position..position + 3]
+      input_1 = mode_1 == 0 ? state[input_1_pos] : input_1_pos
+      input_2 = mode_2 == 0 ? state[input_2_pos] : input_2_pos
+      output = input_1 * input_2
+      # output is always in position mode
+      state[output_pos] = output
+      IntcodeComputer.new(state: state, position: position + 4).run
+    when 3
+      input = 1
+      opcode, output_pos = state[position..position + 1]
+      state[output_pos] = input
+      IntcodeComputer.new(state: state, position: position + 2).run
+    when 4
+      opcode, output_pos = state[position..position + 1]
+      puts state[output_pos]
+      IntcodeComputer.new(state: state, position: position + 2).run
+    when 99
+      return state[0]
+    else
+      raise "invalid opcode #{opcode.inspect}"
+    end
+  end
+
+  def display(state)
+    puts state.inspect
   end
 end
 
-def display(state)
-  puts state.inspect
-end
-
-run(INPUT)
-#run([1002,4,3,4,33])
+IntcodeComputer.new.run
